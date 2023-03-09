ad6e39d859cfc71891df7e33d946e2fe1ed318d8
th4s <th4s@metavoid.xyz>
2023-03-09 15:48:35 +0100

    Improved project structure
    - tokio dependency compiles

--- live-reload.sh
+++ live-reload.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# Watch for changes and recompile and reload the browser
-(trap 'kill 0' SIGINT; cargo watch -- wasm-pack build --target web & live-server --watch="./index.*,./pkg" --entry-file="src/index.html" --browser=firefox)
+# This file is used for hot-reloading during development.
+#
+# It watches for file changes and if a change is detected it recompiles with
+# wasm-pack and hosts it using live-server. Browser is automatically refreshed.
+#
+# Dependencies:
+# Install cargo-watch: cargo install cargo-watch
+# Install live-server: npm install -g live-server
+#
+# Set your browser
+browser="firefox"
+
+# This allows to kill both processes with Ctrl+C
+(trap 'kill 0' SIGINT; cargo watch -- wasm-pack build --target web \
+& live-server --watch="./www/,./pkg/" --entry-file="www/index.html" --browser=$browser)
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn init_panic_hook() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 }
 
 #[wasm_bindgen]
