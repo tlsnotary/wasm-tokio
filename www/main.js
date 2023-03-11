5585ca86281ab9d05ad04045ab9903078763026e
th4s <th4s@metavoid.xyz>
2023-03-11 14:30:21 +0100

    WIP:....

--- www/main.js
+++ www/main.js
@@ -1,1 +1,6 @@
-const myWorker = new Worker('./modules/run.js');
+import init, * as wasm from "./pkg/wasm_tokio.js";
+
+await init();
+wasm.init_panic_hook();
+await wasm.initThreadPool(navigator.hardwareConcurrency);
+wasm.run();
